96deaf1564ce5ff010fe38686c0dce4cc60a24e1
Glitch (notch-arithmetic) <none>
2017-12-29 03:05:54 +0000

    💡💇 Checkpoint server.js:575125

---
+++ server.js
@@ -0,0 +1,25 @@
+
+
+var express = require('express');
+var app = express();
+var multer = require("multer");
+var upload = multer();
+var fs = require('fs');
+
+app.use(express.static('public'));
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.post("/file", upload.single('file'), function(req, res){
+  console.log("Received!");
+  fs.readStream(req.file, function(file){
+  
+  });
+  
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
